89aae546d4c54c338cb9202d446eb4d21d592254
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2020-01-07 09:31:15 +0100

    z

---
+++ README.md
@@ -0,0 +1,1 @@
+https://prod.liveshare.vsengsaas.visualstudio.com/join?7EB79030F2912F49F67AEB529CB709AE9463
---
+++ zajecia-072020.js
@@ -0,0 +1,10 @@
+12
+'string' "string" `string`
+true false
+null
+undefined
+
+var nazwazmiennej = 12;
+let zmienna = 'string';
+
+const STALA = 3.14
