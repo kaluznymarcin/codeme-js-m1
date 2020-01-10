f7e759bc668d50b0823092618108bae36b3bf21b
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2020-01-10 11:21:16 +0100

    zaj

--- kurs_react/src/Currency/Body.js
+++ kurs_react/src/Currency/Body.js
@@ -1,23 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Row from './Row'
 import Cell from './Cell'
 
-const data = [
-    { "currency": "bat (Tajlandia)", "code": "THB", "mid": 0.1266 },
-    { "currency": "dolar amerykański", "code": "USD", "mid": 3.8234 },
-    { "currency": "dolar australijski", "code": "AUD", "mid": 2.6445 }
-];
 
 const createRow = (data) => {
-
-  useEffect(() => {
-    fetch('http://api.nbp.pl/api/exchangerates/tables/a/last/5/', {
-      headers: {
-        Accept: 'application/json'
-      }
-    })
-  });
-
   return data.map( ({ code, mid }, index) => (
     <Row>
       <Cell value={index + 1} modifier="lp" />
@@ -27,8 +13,19 @@ const createRow = (data) => {
   ))
 }
 
-export default () => (
-  <div className="currency__body">
-    {createRow(data)}
-  </div>
-)
+export default () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    data.length === 0 && fetch('http://api.nbp.pl/api/exchangerates/tables/a/last/5/', {
+      headers: {
+        Accept: 'application/json'
+      }
+    }).then(res => res.json()).then(data_from_res => setData(data_from_res[0].rates))
+  });
+  return (
+    <div className="currency__body">
+      {createRow(data)}
+    </div>
+  )
+}
